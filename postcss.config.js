70598832a82669946f297fcb3aa562f102d13fcf
David Mora <dmora@territoriosinteligentes.net>
2025-11-30 19:47:52 -0500

    estructura inicial pagina web

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {}, // 👈 nuevo plugin
+    autoprefixer: {},           // puedes dejarlo, no estorba
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        Hola, soy David – Consultor 👨‍💻
+      </h1>
+    </div>
+  );
+}
+
+export default App;
